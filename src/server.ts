ed17d6a463416b36e55f9eb33b8329a7004f37ce
Fiayz <sifathhossain115@gmail.com>
2025-08-26 19:14:46 +0600

    Feat: schedule call added in server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import app from "./app";
 import { env } from "./config/env";
 import { initDB } from "./config/initDB";
+import { startScheduler } from "./services/schedule.service";
 
 (async () => {
   await initDB();
+  startScheduler();
   app.listen(env.PORT, () => {
     console.log(`🌱 PlantPal API running on http://localhost:${env.PORT}`);
   });
