fe2211d78be7af7329383a00f25886ffe27e020a
Fiayz <sifathhossain115@gmail.com>
2025-08-26 07:12:43 +0600

    Feat: app now has routes index and error handler middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import { env } from "./config/env";
+import router from "./routes";
+import { errorHandler } from "./middlewares/errorHandler";
 
 const app = express();
 
@@ -11,3 +13,8 @@ app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(morgan(env.NODE_ENV === "development" ? "dev" : "combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/api", router);
+app.use(errorHandler);
+
+export default app;
